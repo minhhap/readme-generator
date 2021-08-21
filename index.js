245989faa61742a18d5272ba85b311a8e4085086
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-08-21 11:06:57 -0700

    included packages and example questions

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt(questions)
+};
+
+const questions = [
+    {
+        message:'What is your project title?', 
+        type:'input', 
+        name: 'title'
+      },
+    {
+    message:'What is the project description?', 
+    type:'input', 
+    name: 'description'
+        },
+    {
+        message:'Which license did you use?', 
+        type:'list', 
+        name: 'license',
+        choices:['MIT','Apache','BSD']
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('./README.md', readMeData) {
+    const readMeFile = generatePage(readMeData);
+    fs.writeFile('./README.md',readMeFile) 
+}
+
 
 // TODO: Create a function to initialize app
 function init() {}
