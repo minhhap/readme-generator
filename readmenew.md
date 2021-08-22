581d24a0e67c3aea9fd04cd1542f50a1bf75279f
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-08-21 20:52:59 -0700

    full package for screencast

--- readmenew.md
+++ readmenew.md
@@ -1,8 +1,9 @@
 
-# t 
+# Professional Readme Creator 
 
 ## Description
-d
+An application that allows developers to quickly generate a README
+
 
 ## Table of Content
 * [Description](#description)
@@ -14,22 +15,28 @@ d
 * [Usage](#usage)
 * [Questions](#github)
 
+
 ## Installation
-i
+Use npm init, npm install, npm install inquirer to download Node modules. Then run the app using the command line: node index
+
 
 ## Usage
-u
+Open-source
+
 
 ## Contributing
-t
+This is an example about contribution guide
+
 
 ## Tests
-df
+n/a
+
 
 ## License
 MIT
 
+
 ## Questions
-Contact me at: github.com/fdgdfg
-or email: fgdfg@gmail.com
+Contact me at: github.com/minhhap
+or email: minhha1234@gmail.com
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,11 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title} 
 
 ## Description
 ${data.description}
+
 
 ## Table of Content
 * [Description](#description)
@@ -28,20 +17,26 @@ ${data.description}
 * [Usage](#usage)
 * [Questions](#github)
 
+
 ## Installation
 ${data.installation}
+
 
 ## Usage
 ${data.usage}
 
+
 ## Contributing
 ${data.contributing}
+
 
 ## Tests
 ${data.tests}
 
+
 ## License
 ${data.license}
+
 
 ## Questions
 Contact me at: github.com/${data.github}
@@ -50,7 +45,4 @@ or email: ${data.email}
 `;
 }
 
-
 module.exports = generateMarkdown;
-
-
